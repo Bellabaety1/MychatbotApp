fa00182722ae2ccd07c6cd0acc64f487fcac0f07
Bellabaety1 <95356547+Bellabaety1@users.noreply.github.com>
2023-01-18 14:51:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,53 +8,40 @@ import detectlanguage
 from detectlanguage import simple_detect # import the translator
 from flask_sqlalchemy import SQLAlchemy
 import datetime
-
 from chat import chatBot
 chatBot = chatBot()
-
-
-#import nltk 
-#nltk.download('punkt')
-
+import nltk
+nltk.download('punkt')
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://bella:FeRo9SCIv9KQerWX3ZIyQRgiZNbBN4wp@dpg-cf3ukg94rebfa0qfv8ig-a.oregon-postgres.render.com/chatbot_s1xw"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://chatsql_q20q_user:QPQGWY74BADYtW3Wk9pK2Tiwm0OQFIFj@dpg-cf2kd4h4reb5o4628a6g-a.oregon-postgres.render.com/chatsql_q20q"
-
+postgres://
 db = SQLAlchemy(app)
-
 CORS(app)
-
 ## Database
-
 class Queries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(1000))
     answer = db.Column(db.String(1000))
     language = db.Column(db.String(5))
     timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
 with app.app_context():
     db.create_all()
-
-
+    
 #from flask import get_response
-
 class translator:
     api_url = "https://translate.googleapis.com/translate_a/single"
     client = "?client=gtx&dt=t"
     dt = "&dt=t"
-
     #fROM English to Kinyarwanda
     def translate(text : str , target_lang : str, source_lang : str):
         sl = f"&sl={source_lang}"
         tl = f"&tl={target_lang}"
         r = requests.get(translator.api_url+ translator.client + translator.dt + sl + tl + "&q=" + text)
         return json.loads(r.text)[0][0][0]
-
 # use this link to get your api key https://detectlanguage.com/
 detectlanguage.configuration.api_key = "13e26484ba8a0a3d865573c4868de0a0"
 detectlanguage.configuration.secure = True
-
 def process_question(text : str):
   source_lang = simple_detect(text)
   resp = translator.translate(text=text, target_lang='en', source_lang=source_lang)
@@ -62,9 +49,7 @@ def process_question(text : str):
 def process_answer(text : str, source_lang):
   resp = translator.translate(text=text, target_lang=source_lang, source_lang='en')
   return resp
-
 # create two routes
-
 def preprocessing(text):
     text = text.lower()
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
@@ -72,7 +57,6 @@ def preprocessing(text):
     text = url_pattern.sub(r'', text)
     text = html_pattern.sub(r'', text)
     text = re.sub(r"[^\w\d'\s]+", ' ', text)
-
     return text
 Q = []
 R = []
@@ -83,12 +67,9 @@ def process(QUESTION: str):
     ORIGINAL_RESPONSE = process_answer(RESPONSE, SL)
     R.append(ORIGINAL_RESPONSE)
     return ORIGINAL_RESPONSE, SL
-
-
 @app.route("/",  methods=["GET"])
 def index_get():
     return render_template("index.html")
-
 @app.route("/predict",methods=["POST"])
 def predict():
     text = request.get_json().get("message")
@@ -99,10 +80,7 @@ def predict():
     query = Queries(question=text, answer=response, language=sl)
     db.session.add(query)
     db.session.commit()
-
     return jsonify(message)
-    
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
